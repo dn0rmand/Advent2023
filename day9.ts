9fb22ef8e982561c84fe4a1454a0a8d67a365a08
dnormand <dominique@normand.us>
2023-12-13 22:24:55 -0500

    Reworked day9 to just do it the way Advent of Code describes it. My nice polynomial would have been useful if they had ask to extrapolate to N

--- day9.ts
+++ day9.ts
@@ -1,26 +1,51 @@
-import { Day } from "./tools/day.ts";
-import { Polynomial } from "./tools/polynomial.ts";
+import { Day } from './tools/day.ts';
 
 export class Day9 extends Day {
   constructor() {
     super(9);
   }
 
-  loadInput(): Polynomial[] {
-    const data = this.readDataFile().map((line) => line.split(" ").map((v) => +v));
-    return data.map((values) => new Polynomial(values));
+  loadInput(): number[][] {
+    const data = this.readDataFile().map(line => line.split(' ').map(v => +v));
+    return data;
   }
 
-  part1(input: Polynomial[]): number {
-    const values = input.map((polynomial) => polynomial.get(polynomial.values.length));
-    const answer = values.reduce((a, v) => a + v);
-    return Number(answer);
+  process(values: number[]): number[] {
+    if (values.length === 0) {
+      throw 'Error';
+    }
+    if (!values.some(a => a !== values[0])) {
+      values.push(values[0]);
+      return values;
+    }
+    let newValues = values.reduce((a: number[], v: number, i: number) => {
+      if (i) {
+        a.push(v - values[i - 1]);
+      }
+      return a;
+    }, []);
+
+    newValues = this.process(newValues);
+    const extra = newValues[newValues.length - 1] + values[values.length - 1];
+    values.push(extra);
+    return values;
   }
 
-  part2(input: Polynomial[]): number {
-    const values = input.map((polynomial) => polynomial.get(-1));
+  solve(values: number[]): number {
+    values = this.process(values);
+    return values[values.length - 1];
+  }
+
+  part1(input: number[][]): number {
+    const values = input.map(values => this.solve(values));
     const answer = values.reduce((a, v) => a + v);
-    return Number(answer);
+    return answer;
+  }
+
+  part2(input: number[][]): number {
+    const values = input.map(values => this.solve(values.reverse()));
+    const answer = values.reduce((a, v) => a + v);
+    return answer;
   }
 }
 
