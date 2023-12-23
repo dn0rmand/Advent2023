1d90bdc9957fcabd918b29b7f5ccb821c13e7791
dnormand <dominique@normand.us>
2023-12-23 09:30:55 -0500

    day 21 done

--- day21.ts
+++ day21.ts
@@ -15,6 +15,9 @@ type GardenMap = {
   size: number;
 };
 
+const BLOCK_OFFSET = 2;
+const BLOCK_COUNT = 5;
+
 class Solver {
   map: boolean[][];
   width: number;
@@ -22,11 +25,13 @@ class Solver {
   steps: number;
   states: Map<number, Point>;
   newStates: Map<number, Point>;
+  blocks: Uint16Array[];
   size: number;
 
   constructor(garden: GardenMap, steps: number) {
-    this.states = new Map<number, Point>();
-    this.newStates = new Map<number, Point>();
+    this.states = new Map();
+    this.newStates = new Map();
+    this.blocks = [];
     this.size = garden.size;
     this.steps = steps;
 
@@ -41,15 +46,30 @@ class Solver {
     const x0 = ((x % this.width) + this.width) % this.width;
     const y0 = ((y % this.height) + this.height) % this.height;
 
+    const kx = BLOCK_OFFSET + Math.floor(x / this.width);
+    const ky = BLOCK_OFFSET + Math.floor(y / this.height);
+
     if (this.map[y0][x0]) {
       const key = x + 300 + (y + 300) * 1000;
-      this.newStates.set(key, { x, y });
+
+      if (!this.newStates.has(key)) {
+        this.newStates.set(key, { x, y });
+
+        if (kx >= 0 && kx < BLOCK_COUNT && ky >= 0 && ky < BLOCK_COUNT) {
+          let r = this.blocks[ky];
+          if (!r) {
+            r = this.blocks[ky] = new Uint16Array(BLOCK_COUNT);
+          }
+          r[kx] = (r[kx] || 0) + 1;
+        }
+      }
     }
   }
 
-  solve() {
+  solve(): number {
     for (let step = 1; step <= this.steps; step++) {
       this.newStates.clear();
+      this.blocks = [];
 
       for (const { x, y } of this.states.values()) {
         this.add(x, y - 1);
@@ -60,6 +80,30 @@ class Solver {
 
       [this.states, this.newStates] = [this.newStates, this.states];
     }
+
+    return this.states.size;
+  }
+
+  calculate(steps: number): number {
+    let x = (steps - 65) / 131;
+
+    const A = this.blocks[2][0] + this.blocks[2][4] + this.blocks[0][2] + this.blocks[4][2];
+    const B = this.blocks[0][1] + this.blocks[0][3] + this.blocks[4][1] + this.blocks[4][3];
+    const C = this.blocks[1][1] + this.blocks[1][3] + this.blocks[3][3] + this.blocks[3][1];
+
+    const k1 = this.blocks[2][1];
+    const k2 = this.blocks[2][2];
+
+    let answer = A + x * B + (x - 1) * C;
+
+    let k = k1;
+    while (--x) {
+      answer += x * 4 * k;
+      k = k === k1 ? k2 : k1;
+    }
+    answer += k;
+
+    return answer;
   }
 }
 
@@ -89,141 +133,17 @@ export class Day21 extends Day {
     return { map, start, height: map.length, width: map[0].length, size };
   }
 
-  part1Add(input: GardenMap, visited: Set<number>, states: PointMap, x: number, y: number, remaining: number) {
-    if (x < 0 || y < 0 || y >= input.height || x >= input.width) {
-      return;
-    }
-
-    if (input.map[y][x]) {
-      const key = x + input.width * y;
-      if (visited.has(key)) {
-        return;
-      }
-      if (remaining % 2 === 0) {
-        visited.add(key);
-      }
-      states.set(key, { x, y });
-    }
-  }
-
-  part1Moves(input: GardenMap, steps: number): number {
-    let states: PointMap = new Map();
-    let newStates: PointMap = new Map();
-
-    states.set(0, input.start);
-
-    const visited = new Set<number>();
-
-    for (let i = 1; states.size && i <= steps; i++) {
-      newStates.clear();
-      for (const { x, y } of states.values()) {
-        const key = x + y * input.width;
-        this.part1Add(input, visited, newStates, x, y - 1, steps - i);
-        this.part1Add(input, visited, newStates, x, y + 1, steps - i);
-        this.part1Add(input, visited, newStates, x - 1, y, steps - i);
-        this.part1Add(input, visited, newStates, x + 1, y, steps - i);
-      }
-      [states, newStates] = [newStates, states];
-    }
-    let total = visited.size;
-    states.forEach((_, key) => {
-      if (!visited.has(key)) {
-        total++;
-      }
-    });
-    return total;
-  }
-
   part1(input: GardenMap): number {
-    return this.part1Moves(input, 64);
-  }
-
-  dump(step: number, input: GardenMap, states: Map<number, PointMap>): string {
-    // console.log(`--- STEP: ${step} ---`);
-    let data: string[] = [];
-    for (let y = 0; y < input.height; y++) {
-      for (let x = 0; x < input.width; x++) {
-        const k = `${x}:${y}`;
-        if (states.has(k)) {
-          data.push('O');
-        } else if (input.map[y][x]) {
-          data.push('.');
-        } else {
-          data.push('#');
-        }
-      }
-      // console.log(l.join(''));
-    }
-    return data.join('');
-  }
-
-  part2Add(input: GardenMap, visited: Set<string>, states: Map<string, Point>, x: number, y: number, remaining: number) {
-    const x0 = ((x % input.width) + input.width) % input.width;
-    const y0 = ((y % input.height) + input.height) % input.height;
-
-    const kx = Math.floor(x / input.width);
-    const ky = Math.floor(y / input.height);
-
-    if (input.map[y0][x0]) {
-      const key = `${x}:${y}`;
-
-      if (visited.has(key)) {
-        return;
-      }
-      if (remaining % 2 === 0) {
-        visited.add(key);
-      }
-      states.set(key, { x, y });
-    }
-  }
-
-  part2Moves(input: GardenMap, steps: number): number {
-    let states = new Map<string, Point>();
-    let newStates = new Map<string, Point>();
-
-    states.set('0', input.start);
-
-    const visited = new Set<string>();
-
-    // const freqs = new Map<string, number>();
-
-    for (let i = 1; states.size && i <= steps; i++) {
-      newStates.clear();
-      for (const { x, y } of states.values()) {
-        this.part2Add(input, visited, newStates, x, y - 1, steps - i);
-        this.part2Add(input, visited, newStates, x, y + 1, steps - i);
-        this.part2Add(input, visited, newStates, x - 1, y, steps - i);
-        this.part2Add(input, visited, newStates, x + 1, y, steps - i);
-      }
-      [states, newStates] = [newStates, states];
-      // const key = this.dump(i, input, states);
-      // if (freqs.has(key)) {
-      //   const o = freqs.get(key) || 0;
-      //   console.log(i - o, o);
-      // }
-      // freqs.set(key, i);
-    }
-
-    let total = visited.size;
-    states.forEach((_, key) => {
-      if (!visited.has(key)) {
-        total++;
-      }
-    });
-    return total;
+    return new Solver(input, 64).solve();
   }
 
   part2(input: GardenMap): number {
     const steps = 26501365;
+    const solver = new Solver(input, 327);
 
-    let x = (steps - 65) / 131;
-    let answer = 2 * 5846 + 5869 + 5823 + x * (996 + 990 + 978 + 977) + (x - 1) * (6813 + 6804 + 6790 + 6781);
-    let k = 7757;
-    while (--x) {
-      answer += x * 4 * k;
-      k = k === 7757 ? 7748 : 7757;
-    }
-    answer += k;
+    solver.solve();
+
+    const answer = solver.calculate(steps);
     return answer;
   }
 }
