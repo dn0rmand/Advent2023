cb3f14e83de88fe8f42beeecce00e1ddaf7f0862
dnormand <dominique@normand.us>
2023-12-28 17:29:19 -0500

    day 21 way faster

--- day21.ts
+++ day21.ts
@@ -18,6 +18,7 @@ class Solver {
   height: number;
   steps: number;
   states: Map<number, Point>;
+  visited: Set<number>;
   newStates: Map<number, Point>;
   blocks: Uint16Array[];
   size: number;
@@ -25,6 +26,7 @@ class Solver {
   constructor(garden: GardenMap, steps: number) {
     this.states = new Map();
     this.newStates = new Map();
+    this.visited = new Set();
     this.blocks = [];
     this.size = garden.size;
     this.steps = steps;
@@ -36,40 +38,50 @@ class Solver {
     this.states.set(0, garden.start);
   }
 
-  add(x: number, y: number) {
-    const x0 = ((x % this.width) + this.width) % this.width;
-    const y0 = ((y % this.height) + this.height) % this.height;
-
+  addToBlock(x: number, y: number) {
     const kx = BLOCK_OFFSET + Math.floor(x / this.width);
     const ky = BLOCK_OFFSET + Math.floor(y / this.height);
 
+    if (kx >= 0 && kx < BLOCK_COUNT && ky >= 0 && ky < BLOCK_COUNT) {
+      let r = this.blocks[ky];
+      if (!r) {
+        r = this.blocks[ky] = new Uint16Array(BLOCK_COUNT);
+      }
+      r[kx]++;
+    }
+  }
+
+  add(x: number, y: number, remainingSteps: number) {
+    const x0 = ((x % this.width) + this.width) % this.width;
+    const y0 = ((y % this.height) + this.height) % this.height;
+
     if (this.map[y0][x0]) {
       const key = x + 300 + (y + 300) * 1000;
+      if (this.visited.has(key)) {
+        return;
+      }
+
+      if (remainingSteps % 2 === 0) {
+        this.visited.add(key);
+        this.addToBlock(x, y);
+      }
 
       if (!this.newStates.has(key)) {
         this.newStates.set(key, { x, y });
-
-        if (kx >= 0 && kx < BLOCK_COUNT && ky >= 0 && ky < BLOCK_COUNT) {
-          let r = this.blocks[ky];
-          if (!r) {
-            r = this.blocks[ky] = new Uint16Array(BLOCK_COUNT);
-          }
-          r[kx] = (r[kx] || 0) + 1;
-        }
       }
     }
   }
 
   solve(): number {
+    this.blocks = [];
     for (let step = 1; step <= this.steps; step++) {
       this.newStates.clear();
-      this.blocks = [];
 
       for (const { x, y } of this.states.values()) {
-        this.add(x, y - 1);
-        this.add(x, y + 1);
-        this.add(x - 1, y);
-        this.add(x + 1, y);
+        this.add(x, y - 1, this.steps - step);
+        this.add(x, y + 1, this.steps - step);
+        this.add(x - 1, y, this.steps - step);
+        this.add(x + 1, y, this.steps - step);
       }
 
       [this.states, this.newStates] = [this.newStates, this.states];
@@ -128,7 +140,11 @@ export class Day21 extends Day {
   }
 
   part1(input: GardenMap): number {
-    return new Solver(input, 64).solve();
+    const solver = new Solver(input, 64);
+
+    solver.solve();
+
+    return solver.blocks[2][2];
   }
 
   part2(input: GardenMap): number {
